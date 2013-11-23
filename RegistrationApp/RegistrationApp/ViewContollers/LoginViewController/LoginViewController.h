efab77684ab0d994e9a5abf5a526a76b581977b1
DheerajRathore <rathoredheeraj7@gmail.com>
2013-11-23 11:33:50 +0530

    Adding LoginViewController.

---
+++ RegistrationApp/RegistrationApp/ViewContollers/LoginViewController/LoginViewController.h
@@ -0,0 +1,13 @@
+//
+//  LoginViewController.h
+//  RegistrationApp
+//
+//  Created by geek on 11/23/13.
+//  Copyright (c) 2013 geek. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface LoginViewController : UIViewController
+
+@end
